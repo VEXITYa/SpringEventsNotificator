e226c0a17c865265b23d2b427cd85abeef02094d
vex1t <ya@akuchishkin.ru>
2025-11-23 23:11:56 +0400

    Initial commit

---
+++ src/main/java/dev/kuchishkin/SpringEventsNotificatorApplication.java
@@ -0,0 +1,13 @@
+package dev.kuchishkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventsNotificatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEventsNotificatorApplication.class, args);
+    }
+
+}
